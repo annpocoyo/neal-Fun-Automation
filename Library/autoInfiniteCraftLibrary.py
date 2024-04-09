7d47fb19074542a95d2b69669f71681ac791e8ca
annpocoyo <ryant23@outlook.ie>
2024-04-09 13:47:40 +1000

    Add action to drag a element to another element

--- Library/autoInfiniteCraftLibrary.py
+++ Library/autoInfiniteCraftLibrary.py
@@ -10,6 +10,8 @@ import sys #  ^
 import json # |
 from selenium import webdriver # For Browser Control
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.remote.webelement import WebElement # For intelisense
 
 class autoInfiniteCraftClass:
     """This class contains all functions used by autoinfinitecraft.py"""
@@ -19,5 +21,16 @@ class autoInfiniteCraftClass:
         self.driver = webdriver.Firefox()
         self.driver.get("https://neal.fun/infinite-craft/")
 
+        # Initalise action builder
+        self.builder = ActionChains(self.driver)
+
         # Find item list and get array of item elements
         self.itemList = self.driver.find_elements(By.CLASS_NAME, "items-inner")[0].find_elements(By.CLASS_NAME, "item")
+    
+    def dragElementToOtherElement(self, element: WebElement, target: WebElement):
+        """Drag `element: WebElement` to `target: WebElement`"""
+        # Setup click and drag action
+        self.builder.click_and_hold(element) \
+            .move_to_element(target) \
+            .release(target) \
+            .perform() # Start action
