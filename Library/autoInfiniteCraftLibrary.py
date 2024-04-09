ae45751a6f430e92aa474f89b9eca951498ee54e
annpocoyo <ryant23@outlook.ie>
2024-04-09 16:08:02 +1000

    Migrate library construction to be taken care of by central library

--- Library/autoInfiniteCraftLibrary.py
+++ Library/autoInfiniteCraftLibrary.py
@@ -5,18 +5,17 @@ Inside this library:
 
 autoInfiniteCraftClass - contains all functions used by autoinfinitecraft.py
 """
-from selenium import webdriver # For Browser Control
+from _autoBrowserBase import autoBrowserBase # Base for automatic browser control
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.remote.webelement import WebElement # For intelisense
 
-class autoInfiniteCraftClass:
+class autoInfiniteCraftClass(autoBrowserBase):
     """This class contains all functions used by autoinfinitecraft.py"""
     def __init__(self):
         # Initalize Class
-        # Load driver for infinite craft
-        self.driver = webdriver.Firefox()
-        self.driver.get("https://neal.fun/infinite-craft/")
+        # First we need to contact our parent's constructer
+        super().__init__(url = "https://neal.fun/infinite-craft/")
 
         # Initalise action builder
         self.builder = ActionChains(self.driver)
